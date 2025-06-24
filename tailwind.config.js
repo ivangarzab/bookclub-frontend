429ea3f265eeae910e0b9025ef7b6c3ebad412dd
ivangarzab <ivangb6@gmail.com>
2025-06-24 10:13:09 -0700

    Update font and webpage title

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
     ],
     theme: {
       extend: {
+        fontFamily: {
+          'sans': ['Inter', 'system-ui', 'sans-serif'],
+        },
         animation: {
           'spin-slow': 'spin 3s linear infinite',
         },
